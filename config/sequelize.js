491fc596f79fba114fb094d8ab21b4aa7a5d3943
evgeniy <l760@ya.ru>
2020-01-13 16:59:12 +0500

    base start

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 const config = require('./index');
 
 module.exports = {
-	development: config.db[process.env.SEQUELIZE_CONNECTION],
+	development: config.db[process.env.SEQUELIZE_DB_DEVELOPMENT],
+	production: config.db[process.env.SEQUELIZE_DB_PRODUCTION],
 }
