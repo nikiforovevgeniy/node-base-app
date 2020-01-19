7d7ab482aa75ea32582a17b4b3646b96b305c27a
evgeniy <l760@ya.ru>
2020-01-19 17:57:33 +0500

    change sequelize init

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const config = require('./index');
 
 module.exports = {
-	development: config.db[process.env.SEQUELIZE_DB_DEVELOPMENT],
-	production: config.db[process.env.SEQUELIZE_DB_PRODUCTION],
+	development: config.db[process.env.SEQUELIZE_DB_DEVELOPMENT || 'default'],
+	production: config.db[process.env.SEQUELIZE_DB_PRODUCTION || 'default'],
 }
