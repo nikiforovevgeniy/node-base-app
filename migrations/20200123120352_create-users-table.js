855e3ee6f4322ff6160008c1f2969e10e6510a68
evgeniy <l760@ya.ru>
2020-02-04 10:31:59 +0500

    change users migration

--- migrations/20200123120352_create-users-table.js
+++ migrations/20200123120352_create-users-table.js
@@ -1,9 +1,10 @@
 exports.up = function(knex) {
 	return knex.schema.createTable('users', function(table) {
 		table.increments();
-		table.string('firstName').notNullable();
-		table.string('lastName').notNullable();
+		table.string('name').notNullable();
 		table.string('email').notNullable();
+		table.string('password');
+		table.string('service');
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
 	})
